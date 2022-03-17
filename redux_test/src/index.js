2db7d30c177344ef9ad482557752a0a385eb3483
Voirtheo <464813105@qq.com>
2022-03-17 21:42:49 +0800

    react-redux

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
-//检测redux中的状态改变，redux若变则重新渲染
-store.subscribe(() => {
-  ReactDOM.render(<App/>,document.getElementById('root'))
-})
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
+
+
 
 
