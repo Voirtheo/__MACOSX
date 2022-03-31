c53beb5912a8c1943f574986b35fcd94d0e54137
Voirtheo <464813105@qq.com>
2022-03-31 16:08:13 +0800

    竖条轮播图

--- wangyiyun/src/containers/Home/index.jsx
+++ wangyiyun/src/containers/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Category from "./Category";
+import Headline from "./Headline";
 
 
 function Home(){
@@ -7,6 +8,7 @@ function Home(){
     return(
         <div>
             <Category />
+            <Headline />
         </div>
     )
 }
